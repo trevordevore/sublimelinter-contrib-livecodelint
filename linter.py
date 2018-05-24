a36189574ac93ced134141d7960903f3214cabf2
herr kaste <herr.kaste@gmail.com>
2018-05-24 15:47:54 +0200

    Cleanup
    Hi! We want to change the signature of `__init__`, this would break your plugin, and while I'm here I make following suggestions.
    
    I coded this in the Github editor so it might be not completely okay. Also no docs bc I can only edit one file here anyway.
    
    - The common setting for the binary is called `executable`. `executable` is built into SublimeLinter and could replace your 'livecode-server-path'. The default here would be 'livecode-community-server' and if it's on PATH the user would be ready to go without editing the settings.
    
    - 'scope' and 'explicitVariable' are normal cli args and can be expressed within `defaults`. But users must then use this longer name instead of 'explicitvars'
    
    - Removed some deprecated members and the ones holding the default values.
    
    :wave:

--- linter.py
+++ linter.py
@@ -10,52 +10,23 @@
 
 """This module exports the Livecodelint plugin class."""
 import os
-from SublimeLinter.lint import Linter, util
+from SublimeLinter.lint import Linter
+
+
+LIVECODE_SCRIPT = os.path.join(os.path.dirname(__file__), 'livecodelint.lc')
 
 
 class Livecodelint(Linter):
     """Provides an interface to livecodelint."""
 
-    syntax = ('livecode')
-    cmd = None
-    executable = None
-    version_args = '--version'
-    version_re = r'(?P<version>\d+\.\d+\.\d+)'
-    version_requirement = ''
+    cmd = ('livecode-community-server', LIVECODE_SCRIPT, '$args')
     regex = r'(?P<line>\d+),(?P<col>\d+),(?P<message>.+)'
     multiline = False
     line_col_base = (1, 1)
-    tempfile_suffix = None
-    error_stream = util.STREAM_BOTH
-    selectors = {}
     word_re = None
-    defaults = {}
-    inline_settings = None
-    inline_overrides = None
-    comment_re = None
 
-    """ Variables set by user """
-    livecode_path = ''
-    explicit_vars = False
-
-    """ Store path to LiveCode script that acts as bridge between livecode community server and SublimeLinter """
-    livecodelint_path = ''
-
-    def __init__(self, view, syntax):
-        """Initialize and load lc-community-server from settings if present."""
-
-        Linter.__init__(self, view, syntax)
-
-    def run(self, cmd, code):
-        """Check syntax with lc-community-server."""
-        
-        self.livecode_path = self.get_view_settings().get('livecode-server-path')
-        self.explicit_vars = self.get_view_settings().get('explicitvars')
-        self.livecodelint_path = os.path.dirname(__file__) + '/livecodelint.lc'
-
-        cmd = [
-            self.livecode_path,
-            self.livecodelint_path,
-            '-scope=.source.livecodescript',
-            '-explicitVariables=' + str(self.explicit_vars).lower()]
-        return self.communicate(cmd, code)
+    defaults = {
+        'selector': 'source.livecode',
+        '-scope=': '.source.livecodescript',
+        '-explicitVariables=': False
+    }
